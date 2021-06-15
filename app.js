115fe95113d7efd450a48122f9a26ecc380d5ab1
AlTsao <al.tsao.yw@gmail.com>
2021-06-15 09:42:14 +0800

    draw img

--- app.js
+++ app.js
@@ -1,4 +1,58 @@
 async function draw() {
+  // Data
+  const dataset = await d3.csv('covid19_daily_test.csv')
+
+  // Data Parse
+  const parseData = d3.timeParse('%Y/%m/%d')
+  const xAccessor = d => parseData(d.date)
+  const yAccessor = d => parseInt(d.testing)
+
+
+  // Dimensions
+  let dimensions = {
+    width: 1000,
+    height: 500,
+    margins: 50,
+  };
+
+  dimensions.ctrWidth = dimensions.width - dimensions.margins * 2
+  dimensions.ctrHeight = dimensions.height - dimensions.margins * 2
+
+  // Draw Ctr
+  const svg = d3.select('#chart')
+    .append("svg")
+    .attr("width", dimensions.width)
+    .attr("height", dimensions.height)
+
+  const ctr = svg.append("g") // <g>
+    .attr(
+      "transform",
+      `translate(${dimensions.margins}, ${dimensions.margins})`
+    )
+
+  // Scales
+  const xScale = d3.scaleUtc()
+    .domain(d3.extent(dataset, xAccessor))
+    .range([0, dimensions.ctrWidth])
+
+  const yScale = d3.scaleLinear()
+    .domain(d3.extent(dataset, yAccessor))
+    .range([dimensions.ctrHeight, 0])
+    .nice()
+
+  // Draw Img
+  const lineGenerator = d3.line()
+    .x((d) => xScale(xAccessor(d)))
+    .y((d) => yScale(yAccessor(d)))
+
+  ctr.append('path')
+    .datum(dataset)
+    .attr('d', lineGenerator)
+    .attr('fill', 'none')
+    .attr('stroke', '#30475e')
+    .attr('stroke-width', 2)
+
+  // Axis
 
 
 }
