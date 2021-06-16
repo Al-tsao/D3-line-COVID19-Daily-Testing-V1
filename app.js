3a30b655db3eece16907338baddc0d5d10364438
AlTsao <al.tsao.yw@gmail.com>
2021-06-16 09:38:14 +0800

    add axis

--- app.js
+++ app.js
@@ -12,13 +12,13 @@ async function draw() {
   let dimensions = {
     width: 1000,
     height: 500,
-    margins: 50,
+    margins: 100,
   };
 
   dimensions.ctrWidth = dimensions.width - dimensions.margins * 2
   dimensions.ctrHeight = dimensions.height - dimensions.margins * 2
 
-  // Draw Ctr
+  // Ctr
   const svg = d3.select('#chart')
     .append("svg")
     .attr("width", dimensions.width)
@@ -40,7 +40,7 @@ async function draw() {
     .range([dimensions.ctrHeight, 0])
     .nice()
 
-  // Draw Img
+  // Img
   const lineGenerator = d3.line()
     .x((d) => xScale(xAccessor(d)))
     .y((d) => yScale(yAccessor(d)))
@@ -53,6 +53,55 @@ async function draw() {
     .attr('stroke-width', 2)
 
   // Axis
+  // Y axis
+  const yAxis = d3.axisLeft(yScale)
+    .tickFormat((d) => {
+      const addComma = d3.format(",")
+      const newFormat = addComma(d)
+      return `${newFormat}`
+    })
+
+  const yAxisGroup = ctr.append('g')
+    .call(yAxis)
+    .classed('axis', true)
+
+  yAxisGroup.append('text')
+    .attr('y', -60)
+    .attr('x', -(dimensions.ctrHeight / 2))
+    .attr('fill', 'black')
+    .text('Number')
+    .style('transform', 'rotate(270deg)')
+    .style('text-anchor', 'middle')
+
+  // X axis
+  const xAxis = d3.axisBottom(xScale)
+    .tickFormat((d) => {
+      dateFormatter = d3.timeFormat('%Y/%m/%d')
+      date = dateFormatter(d)
+      return date
+    })
+    // .ticks(3)
+    .tickValues([
+      parseData('2020/03/01'),
+      parseData('2020/06/01'),
+      parseData('2020/09/01'),
+      parseData('2020/12/01'),
+      parseData('2021/03/01'),
+      parseData('2021/06/01'),
+    ])
+
+
+  const xAxisGroup = ctr.append('g')
+    .style('transform', `translateY(${dimensions.ctrHeight}px)`)
+    .call(xAxis)
+    .classed('axis', true)
+
+  xAxisGroup.append('text')
+    .attr('x', dimensions.ctrWidth / 2)
+    .attr('y', 35)
+    .attr('fill', 'black')
+    .text('Testing')
+
 
 
 }
